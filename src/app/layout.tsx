3f2dcdfc387bf8fe2088cfddef88e641952a760c
Mohammad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2025-03-02 19:04:11 +0330

    [#2-IMPLEMENT] add header component (#3)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Public_Sans } from "next/font/google";
 import "./globals.scss";
 import "bootstrap/dist/css/bootstrap.css";
 import BootstrapClient from "@/(Components)/BootstrapClient";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const publicSans = Public_Sans({
+  weight: ["400", "500", "600", "700", "800"],
   subsets: ["latin"],
 });
 
@@ -26,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${publicSans.className} `}>
         {children}
         <BootstrapClient />
       </body>
