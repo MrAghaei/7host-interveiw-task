a98fa315c99e3b243ffb969950670a71f432e1d0
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2025-03-02 20:19:37 +0330

    [#2-IMPLEMENT] update header

---
+++ src/(Components)/Header/Header.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import style from "./Header.module.scss";
+function Header() {
+  return (
+    <header className="py-custom-3 border-bottom border-custom-border">
+      <div className="container mx-auto row align-items-center">
+        <div className="d-flex col gap-custom-25 align-items-center">
+          {/* Logo */}
+          <div className="col-auto">
+            <Image src="/logo.png" alt="logo" width={77} height={20} />
+          </div>
+
+          {/* Navigation Links (center) */}
+          <div className="col d-flex gap-4">
+            <button className="btn text-custom-light-text1 fw-semibold">
+              Pricing
+            </button>
+            <button className="btn text-custom-light-text1 fw-semibold">
+              How it Works
+            </button>
+            <button className="btn text-custom-light-text1 fw-semibold">
+              FAQ
+            </button>
+            <button className="btn text-custom-secondary fw-semibold">
+              Currencies
+            </button>
+          </div>
+        </div>
+
+        {/* Sign In and Get Started (right) */}
+        <div className="col-auto d-flex gap-3">
+          <button className="btn text-custom-light-text1 fw-semibold">
+            Sign In
+          </button>
+          <button
+            className={`btn btn-custom-primary ${style.btnSizingPrimary}`}
+          >
+            Get Started
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
