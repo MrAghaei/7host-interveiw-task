4a0d5c2fb6c0b1c45cafa30e038fc552633986de
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2025-03-04 14:46:08 +0330

    [#14-IMPLEMENT] currency list

--- src/app/currencies/page.tsx
+++ src/app/currencies/page.tsx
@@ -1,11 +1,17 @@
 import Header from "@/(Components)/Header/Header";
 import CurrencyHero from "@/(Components)/CurrencyHero/CurrencyHero";
+import CallToAction from "@/(Components)/CallToAction/CallToAction";
+import Footer from "@/(Components)/Footer/Footer";
+import CurrencyList from "@/(Components)/CurrencyList/CurrencyList";
 
 function Page() {
   return (
     <>
       <Header />
       <CurrencyHero />
+      <CurrencyList />
+      <CallToAction />
+      <Footer />
     </>
   );
 }
