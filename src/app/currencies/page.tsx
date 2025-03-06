9d78780f494e8a394fa857ab662e94ac2476e642
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2025-03-06 11:36:23 +0330

    [#22-UPDATE] update currency list page

--- src/app/currencies/page.tsx
+++ src/app/currencies/page.tsx
@@ -1,13 +1,13 @@
-import HeaderDesktop from "@/(Components)/HeaderDesktop/HeaderDesktop";
 import CurrencyHero from "@/(Components)/CurrencyHero/CurrencyHero";
 import CallToAction from "@/(Components)/CallToAction/CallToAction";
 import Footer from "@/(Components)/Footer/Footer";
 import CurrencyList from "@/(Components)/CurrencyList/CurrencyList";
+import Header from "@/(Components)/Header/Header";
 
 function Page() {
   return (
     <>
-      <HeaderDesktop />
+      <Header />
       <CurrencyHero />
       <CurrencyList />
       <CallToAction />
